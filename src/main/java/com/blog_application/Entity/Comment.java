daa320dcc8b05d9239781a4dad87d47f8f1d3784
Sarthak Pawar <pawarsr06@gmail.com>
2025-02-21 23:15:41 +0530

    Updated features in Comment Entity

--- src/main/java/com/blog_application/Entity/Comment.java
+++ src/main/java/com/blog_application/Entity/Comment.java
@@ -8,17 +8,12 @@ import java.util.Date;
 @Entity
 @Data
 public class Comment {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String content;
-
     private Date createdAt;
-
     private  String postedBy;
-
     @ManyToOne
     @JoinColumn(name = "post_id",nullable = false)
     private Post post;
