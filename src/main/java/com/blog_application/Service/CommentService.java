a53cecd6b85a82ad26b8cd982049c6385529212d
Sarthak Pawar <pawarsr06@gmail.com>
2025-02-21 23:19:54 +0530

    Updated features in CommentService Implementation

--- src/main/java/com/blog_application/Service/CommentService.java
+++ src/main/java/com/blog_application/Service/CommentService.java
@@ -10,5 +10,6 @@ import java.util.List;
 @Repository
 public interface CommentService {
     Comment createComment(Long postId,String postedBy,String content);
+
     List<Comment> getCommentsByPostId(Long postId);
 }
--- src/main/java/com/blog_application/Service/CommentServiceImpl.java
+++ src/main/java/com/blog_application/Service/CommentServiceImpl.java
@@ -7,20 +7,16 @@ import com.blog_application.Repository.PostRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class CommentServiceImpl implements CommentService {
-
     @Autowired
     private CommentRepository commentRepository;
-
     @Autowired
     private PostRepository postRepository;
-
     public Comment createComment(Long postId,String postedBy,String content){
         Optional<Post>optionalPost=postRepository.findById(postId);
         if(optionalPost.isPresent()){
@@ -29,26 +25,12 @@ public class CommentServiceImpl implements CommentService {
             comment.setContent(content);
             comment.setPostedBy(postedBy);
             comment.setCreatedAt(new Date());
-
             return commentRepository.save(comment);
         }
         throw new EntityNotFoundException("Post not Found");
     }
 
-
     public List<Comment> getCommentsByPostId(Long postId){
         return commentRepository.findByPostId(postId);
     }
-
-
-
-
-
-
-
-
-
-
-
-
 }
